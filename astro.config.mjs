6d1a124cd13850ebe49dbe4cd9549b9bd061cca6
Oliver Rimmer <oliver.rimmer@onetrading.com>
2025-10-31 20:34:23 +0000

    Configure Astro for hybrid output to support API routes

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,4 +5,5 @@ import tailwind from '@astrojs/tailwind';
 // https://astro.build/config
 export default defineConfig({
   integrations: [tailwind()],
+  output: 'hybrid', // Enable hybrid mode for API routes
 });
